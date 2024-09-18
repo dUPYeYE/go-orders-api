a9d15dda56be190a7ca578b90303cb676bae5b4a
robi-dupp <boros.robert@fxgarage.eu>
2024-09-18 17:43:51 +0300

    All methods handled

--- model/order.go
+++ model/order.go
@@ -10,7 +10,7 @@ type Order struct {
   OrderID       uint64     `json:"order_id"`
   CustomerID    uuid.UUID  `json:"customer_id"`
   LineItems     []LineItem `json:"line_items"`
-  CreadtedAt    *time.Time `json:"created_at"`
+  CreatedAt     *time.Time `json:"created_at"`
   ShippedAt     *time.Time `json:"shipped_at"`
   CompletedAt   *time.Time `json:"completed_at"`
 }
